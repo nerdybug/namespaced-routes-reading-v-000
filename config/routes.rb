d5726c5504699e3b546d61732247306311ea71e2
Meredith Reece <mardebugger@gmail.com>
2018-08-05 01:11:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
   scope '/admin' do
     resources :stats, only: [:index]
   end
